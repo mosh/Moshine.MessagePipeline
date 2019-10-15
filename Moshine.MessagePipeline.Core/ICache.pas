8f7f0bc353ef8379d200bc6e4d835be19d9566cc
John Moshakis <john.moshakis@firmex.com>
2019-10-15 00:05:44 -0400

    Make more dynamic obsolete

--- Moshine.MessagePipeline.Core/ICache.pas
+++ Moshine.MessagePipeline.Core/ICache.pas
@@ -4,6 +4,7 @@ type
 
   ICache = public interface
     method Add(key:String;value:Object);
+    [Obsolete('Use generic version instead')]
     method Get(key:String):dynamic;
     method Get<T>(key:String):T;
   end;
