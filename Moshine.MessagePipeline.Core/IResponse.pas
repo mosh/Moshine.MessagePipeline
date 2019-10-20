5831e7e6ae3d108d0c38cf7773f40042159c1392
John Moshakis <john.moshakis@firmex.com>
2019-10-20 17:00:41 -0400

    Remove obsolete code

--- Moshine.MessagePipeline.Core/IResponse.pas
+++ Moshine.MessagePipeline.Core/IResponse.pas
@@ -6,12 +6,7 @@ uses
 type
 
   IResponse = public interface
-    [Obsolete('Use generic version instead')]
-    method WaitForResultAsync(cache:ICache):Task<dynamic>;
     method WaitForResultAsync<T>(cache:ICache):Task<T>;
-    [Obsolete('Use async generic version instead')]
-    method WaitForResult(cache:ICache):dynamic;
-    [Obsolete('Use async version instead')]
     method WaitForResult<T>(cache:ICache):T;
   end;
 
--- Moshine.MessagePipeline/Response.pas
+++ Moshine.MessagePipeline/Response.pas
@@ -14,6 +14,8 @@ type
   private
     class property Logger: Logger := LogManager.GetCurrentClassLogger;
   public
+    property MaximumWaitTimeInSeconds:Integer := 30;
+
     property Id:Guid;
 
     method WaitForResultAsync<T>(cache:ICache):Task<T>;
@@ -36,7 +38,7 @@ type
 
       var cancelTask := Task.Factory.StartNew(() ->
         begin
-          Thread.Sleep(new TimeSpan(0,0,30));
+          Thread.Sleep(new TimeSpan(0,0,MaximumWaitTimeInSeconds));
           source.Cancel;
         end,
         token);
@@ -53,104 +55,11 @@ type
     end;
 
 
-
-    [Obsolete('Use generic version instead')]
-    method WaitForResultAsync(cache:ICache):Task<dynamic>;
-    begin
-      Logger.Trace('Started');
-
-      var source := new CancellationTokenSource;
-      var token := source.Token;
-      var obj:Object := nil;
-
-      var pollingTask := Task.Factory.StartNew(() ->
-        begin
-
-          repeat
-            token.ThrowIfCancellationRequested;
-            obj := cache.Get(Id.ToString);
-            until (assigned(obj));
-        end,
-        token);
-
-      var cancelTask := Task.Factory.StartNew(() ->
-        begin
-          Thread.Sleep(new TimeSpan(0,0,30));
-          source.Cancel;
-        end,
-        token);
-
-      await Task.WhenAny(cancelTask,pollingTask);
-
-      if((pollingTask.IsCompleted) and (not pollingTask.IsCanceled) and (not pollingTask.IsFaulted))then
-      begin
-        Logger.Trace('Returning value');
-        exit obj;
-      end;
-      Logger.Trace('Returning nil');
-      exit nil;
-
-    end;
-
     method WaitForResult<T>(cache:ICache):T;
     begin
-      Logger.Trace('Started waiting');
-      var pollingTask := Task.Factory.StartNew(() ->
-      begin
-        var obj:T := nil;
-        var startTime:=DateTime.Now;
-        var difference:TimeSpan;
-        repeat
-          obj:=cache.Get<T>(Id.ToString);
-          difference:=DateTime.Now.Subtract(startTime);
-        until (assigned(obj)) or (difference.TotalSeconds > 30);
-
-        Logger.Trace($'Returning result assigned {assigned(obj)}');
-
-        exit obj;
-      end
-      );
-
-      pollingTask.Wait;
-
-      if(assigned(pollingTask.Result))then
-      begin
-        Logger.Trace($'Returning result assigned {assigned(pollingTask.Result)}');
-        exit pollingTask.Result;
-      end;
-      Logger.Trace('Returning no result');
-      exit nil;
-
+      exit WaitForResultAsync<T>(cache).Result;
     end;
 
-    [Obsolete('Use generic version instead')]
-    method WaitForResult(cache:ICache):dynamic;
-    begin
-      Logger.Trace('Started waiting');
-      var pollingTask := Task.Factory.StartNew(() ->
-      begin
-        var obj:Object := nil;
-        var startTime:=DateTime.Now;
-        var difference:TimeSpan;
-        repeat
-          obj:=cache.Get(Id.ToString);
-          difference:=DateTime.Now.Subtract(startTime);
-        until (assigned(obj)) or (difference.TotalSeconds > 30);
-        Logger.Trace($'Returning result assigned {assigned(obj)}');
-        exit obj;
-      end
-      );
-
-      pollingTask.Wait;
-
-      if(assigned(pollingTask.Result))then
-      begin
-        Logger.Trace($'Returning result assigned {assigned(pollingTask.Result)}');
-        exit pollingTask.Result;
-      end;
-      Logger.Trace('Returning no result');
-      exit nil;
-    end;
 
   end;
 
