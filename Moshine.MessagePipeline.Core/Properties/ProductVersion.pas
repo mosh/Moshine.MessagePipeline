7607c86c2f70301aca64cba50a46905b48a8ff62
John Moshakis <john@moshakis.com>
2026-03-07 21:57:23 -0500

    changed version

--- Moshine.MessagePipeline.Core/Properties/ProductVersion.pas
+++ Moshine.MessagePipeline.Core/Properties/ProductVersion.pas
@@ -3,9 +3,9 @@
 uses
   system.Reflection;
 
-[assembly: AssemblyVersion('7.0.1')]
-[assembly: AssemblyFileVersion('7.0.1')]
-[assembly: AssemblyCopyright('Copyright © 2024')]
+[assembly: AssemblyVersion('7.0.2')]
+[assembly: AssemblyFileVersion('7.0.2')]
+[assembly: AssemblyCopyright('Copyright © 2026')]
 [assembly: AssemblyCompany('Moshine')]
 [assembly: AssemblyProduct('Moshine MessagePipeline')]
 end.
